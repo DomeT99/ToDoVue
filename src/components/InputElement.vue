0ad4904aa0e4cc11f0ce1684fa516fd2921fe648
DomeT99 <domenicotenace99@gmail.com>
2022-02-14 18:04:22 +0100

    Creato e stilizzato input

---
+++ src/components/InputElement.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="m-top-20">
+    <v-text-field
+      background-color="amber lighten-2"
+      color="black"
+      solo
+      v-model="itemInsert"
+      append-icon="mdi-lead-pencil"
+     
+      clearable
+      clear-icon="mdi-close-circle-outline"
+    >
+    </v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      itemInsert: "",
+    };
+  },
+ /*  methods: {
+    writeItem() {
+      //let item = this.itemInsert;
+ 
+      
+    },
+  }, */
+};
+</script>
--- src/views/ListToDo.vue
+++ src/views/ListToDo.vue
@@ -1,20 +1,24 @@
 <template>
   <v-container>
     <banner :iconList="icona">Cosa farai oggi?</banner>
+    
+    <input-element></input-element>
   </v-container>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import InputElement from "../components/InputElement.vue";
 export default {
   name: "ToDo",
   data() {
     return {
-      icona: "mdi-checkbox-marked-circle-outline"
+      icona: "mdi-checkbox-marked-circle-outline",
     };
   },
   components: {
     Banner,
+    InputElement,
   },
 };
 </script>
