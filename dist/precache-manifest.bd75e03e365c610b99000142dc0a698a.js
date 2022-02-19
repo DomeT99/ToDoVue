740b3afdbdbf2c5bc0f1a9588f0446f54c8b5e7b
DomeT99 <domenicotenace99@gmail.com>
2022-02-19 17:05:49 +0100

    Build

---
+++ dist/precache-manifest.bd75e03e365c610b99000142dc0a698a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907b3be62bd93e0f1096",
+    "url": "/css/app.86d6470e.css"
+  },
+  {
+    "revision": "2bcc3a5cc3ba07a27587",
+    "url": "/css/chunk-vendors.7a00d0f1.css"
+  },
+  {
+    "revision": "bba15f94c84fc773d329de4708ad054f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "907b3be62bd93e0f1096",
+    "url": "/js/app.2fb5cd0e.js"
+  },
+  {
+    "revision": "2bcc3a5cc3ba07a27587",
+    "url": "/js/chunk-vendors.9b21dd7e.js"
+  },
+  {
+    "revision": "6ab4156a6f3ce6c1e0da26bacc034d07",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
