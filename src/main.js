906506620b2318f39a807f28ae9b35fdb2eb3b62
DomeT99 <domenicotenace99@gmail.com>
2022-03-09 12:34:50 +0100

    Aggiunto vue-touch

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import global from './assets/global.scss'
+import Vue from "vue";
+import App from "./App.vue";
+import Vue2TouchEvents from "vue2-touch-events";
+import "./registerServiceWorker";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import global from "./assets/global.scss";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  Vue2TouchEvents,
   vuetify,
   global,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
