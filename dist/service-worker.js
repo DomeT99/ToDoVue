44d34186914dc0997ca174e31ac570f8a31e1c86
DomeT99 <domenicotenace99@gmail.com>
2022-03-10 12:09:58 +0100

    Build test

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d74b45e56d2af5f35d8364555dba318.js"
+  "/precache-manifest.3455db0e27c8c62dba6720fc7681d513.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "todolist"});
