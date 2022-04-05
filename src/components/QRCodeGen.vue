4d82e766ac0f8094f7a5cfee9e5803cc4df17f2c
DomeT99 <domenicotenace99@gmail.com>
2022-04-05 19:23:00 +0200

    Creati componenti per la generazione Qrcode e aggiunta libreria

---
+++ src/components/QRCodeGen.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-main>
+    <div class="flex-center">
+      <v-card class="mx-auto" max-width="344">
+        <v-card-title>Il tuo QR Code</v-card-title>
+        <v-card-text>{{ qrcode }}</v-card-text>
+      </v-card>
+    </div>
+  </v-main>
+</template>
+
+<script>
+export default {
+  props: {
+    qrcode: Object,
+  },
+};
+</script>
--- src/views/QrCode.vue
+++ src/views/QrCode.vue
@@ -1,9 +1,22 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <Banner>Genera un QR Code</Banner>
+    <InputQrCode></InputQrCode>
+
+    <QRCodeGen></QRCodeGen>
+  </v-container>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue";
+import InputQrCode from "../components/InputQrCode.vue";
+import QRCodeGen from "../components/QRCodeGen.vue";
 export default {
-
-}
+  components: {
+    Banner,
+    InputQrCode,
+    QRCodeGen,
+  },
+  methods: {},
+};
 </script>
