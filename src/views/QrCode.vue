2c645a8075450b8a1a1baaf29841bd28a585987c
DomeT99 <domenicotenace99@gmail.com>
2022-04-10 23:10:12 +0200

    Salvataggio Qrcode

--- src/views/QrCode.vue
+++ src/views/QrCode.vue
@@ -12,7 +12,10 @@
         @ShowAlert="showPopup = $event"
         @GeneraQr="txtQr = $event"
       ></InputQrCode>
-      <QRCodeGen :txtQrcode="txtQr"></QRCodeGen>
+      <QRCodeGen class="mb-8" :txtQrcode="txtQr"></QRCodeGen>
+      <div class="d-flex justify-center">
+        <ButtonSave :saveElement="downloadFile">Salva</ButtonSave>
+      </div>
     </section>
   </v-container>
 </template>
@@ -23,6 +26,7 @@ import InputQrCode from "../components/InputQrCode.vue";
 import QRCodeGen from "../components/QRCodeGen.vue";
 import LoadingCircle from "../components/LoadingCircle.vue";
 import PopUpAlert from "../components/PopUpAlert.vue";
+import ButtonSave from "../components/NotesButtonSave.vue";
 export default {
   components: {
     Banner,
@@ -30,6 +34,7 @@ export default {
     QRCodeGen,
     LoadingCircle,
     PopUpAlert,
+    ButtonSave,
   },
   data() {
     return {
@@ -37,6 +42,44 @@ export default {
       showLoading: true,
       showPopup: false,
     };
+  },
+  methods: {
+    downloadFile() {
+      if (this.txtQr != "Ciao User!" && this.txtQr != "") {
+        const image = document.querySelector("img").src;
+
+        let newFile = this.dataURLtoFile(image, "image");
+
+        const blob = new Blob([newFile], { type: "image/png" });
+        const href = URL.createObjectURL(blob);
+        const a = Object.assign(document.createElement("a"), {
+          href,
+          style: "display:none",
+          download: "QrCode.png",
+        });
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(href);
+        a.remove();
+      } else {
+        this.showPopup = true;
+      }
+    },
+    //Converte la base64 in file
+    dataURLtoFile(dataurl, filename) {
+      debugger;
+      var arr = dataurl.split(","),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+
+      return new File([u8arr], filename, { type: mime });
+    },
   },
   mounted() {
     setTimeout(() => {
